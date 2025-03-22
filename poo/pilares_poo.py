edc96ea1336cdf73a29fa91da38d06ea0a2b290c
Erick-A-Martins <erickalvesqmartins@gmail.com>
2025-03-22 13:43:20 -0300

    add abstraction

--- poo/pilares_poo.py
+++ poo/pilares_poo.py
@@ -54,3 +54,32 @@ print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
 conta.sacar(valor=200)
 
 conta_do_zezinho = ContaBancaria(saldo=50)
+
+
+print("\nExemplo de abstração:")
+from abc import ABC, abstractmethod
+
+class Veiculo(ABC):
+
+    @abstractmethod
+    def ligar(self):
+        pass
+
+    @abstractmethod
+    def desligar(self):
+        pass
+
+class Carro(Veiculo):
+    def __init__(self) -> None:
+        pass
+
+    def ligar(self):
+        # Ligação do carro
+        return "Carro ligado usando a chave"
+    
+    def desligar(self):
+        return "Carro desligado usando a chave"
+
+carro_amarelo = Carro()
+print(carro_amarelo.ligar())
+print(carro_amarelo.desligar())
